a78bef2d041b5c59c42f4b0929a9b79fd2646b47
Estebansito1 <estebansito619@gmail.com>
2025-11-12 00:24:50 -0800

    update

--- Trees.cpp
+++ Trees.cpp
@@ -1,4 +1,4 @@
-/*The purpose of this code is to show how Binary Search Tree works.
+/*The purpose of this program is to show how Binary Search Tree works.
  * A BST is a data structure that organizes numbers so that every node that is smaller than
  * the node goes to the left and values larger goes to the right.
  * This program allows you to insert values search values if they exist and print the sorted values
@@ -10,7 +10,9 @@ using namespace std; //so I dont have to use std:: everytime when cout/cin endl
 // node structure represents each element in the BST
     struct Node
     {
-        int key;
-        Node* left;
-        Node* right;
+        int key; // stores the integers value on this node = key
+        Node* left; // pointer to the left child, smaller value
+        Node* right; // pointer to the right child larger value
+
+        Node(int k) : key(k), left(nullptr), right(nullptr) {}
     };
