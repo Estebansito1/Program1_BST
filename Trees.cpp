ae663524db68b205f36844a2b5b3761221015de4
Estebansito1 <estebansito619@gmail.com>
2025-11-12 21:34:58 -0800

    Merge remote-tracking branch 'origin/master'

--- Trees.cpp
+++ Trees.cpp
@@ -14,5 +14,79 @@ using namespace std; //so I dont have to use std:: everytime when cout/cin endl
         Node* left; // pointer to the left child, smaller value
         Node* right; // pointer to the right child larger value
 
+        // the constructure initializes a node with the given key, with no child's yet
         Node(int k) : key(k), left(nullptr), right(nullptr) {}
     };
+/////////////
+
+// Function to insert a new key into the BST
+    Node* insert(Node* root, int key)
+    {
+        if (!root) return new Node(key); // if the tree is empty, create a ew node
+        if (key < root->key)
+            root-> left= insert(root->left, key); // if smaller, inser to the left
+        else if (key > root->key)
+            root->right = insert(root->right, key); // if large, insert to the right
+        return root; // return the unchanged root node
+    }
+//////////////
+
+//functio to search for a specific key
+    bool search(Node* root, int key)
+    {
+        if (!root) return false; // the tree is empty or did not reach a leaf
+        if (key == root->key) return true; // key was found
+        if (key < root-> key)
+            return search(root-> left, key); //is smaller search the left side
+        return search(root->right, key); // otherwise search the right side
+    }
+///////////////
+
+// function to print the BST in sorte order using the inorder traversal
+    void inorder(Node* root)
+    {
+        if (!root) return; // empty subtree
+        inorder(root->left); // visit the left child first
+        cout<<root->key<<" "; // print the current values
+        inorder(root->right); // visit the right child last
+    }
+//////////////
+
+// the main program
+    int main()
+    {
+        Node* root = nullptr; // start with an empty tree
+        int vals[5] = {50,30,70,20,40}; // array of values to insert into the tree
+        for (int v : vals) // loop through the aaray
+            root = insert (root, v); // insert each number into the BST
+
+        cout << "BST inorder sorted: "; //print
+        inorder(root); cout << "\n"; //display number in sorted order
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
